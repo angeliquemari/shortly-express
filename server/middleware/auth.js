1bd425e493dbffd0c795e3d66a5812114151f4ce
angeliquemari <angelique@nehmzow.com>
2019-09-28 12:28:15 -0700

    add cookie parser and initial session handling

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,10 +1,31 @@
 const models = require('../models');
 const Promise = require('bluebird');
+var cookieParser = require('./cookieParser.js');
 
 module.exports.createSession = (req, res, next) => {
+  cookieParser(req, res, () => {
+    if (Object.keys(req.cookies).length === 0) {
+      models.Sessions.create()
+        .then( (data) => {
+          //get request on data.insertId
+          models.Sessions.get({id: data.insertId})
+            .then( (data) => {
+              req.session = {};
+              req.session.hash = data.hash;
+              res.cookies['shortlyid'] = data.hash;
+              console.log('RES.COOKIES: ', res.cookies);
+              next();
+            });
+        });
+    }
+  });
 };
 
 /************************************************************/
 // Add additional authentication middleware functions below
 /************************************************************/
+
+
+//call parser, if !cookieParser, create new cookie via session.create
+
 
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,17 @@
 const parseCookies = (req, res, next) => {
+  const cookiesObj = {};
+  if (req.headers.cookie) {
+    // Split incoming cookies
+    const cookiesString = req.headers.cookie;
+    const cookiesArr = cookiesString.split('; ');
+    // Loop through cookies array to build cookies Obj
+    for (let i = 0; i < cookiesArr.length; i++) {
+      const splitCookie = cookiesArr[i].split('=');
+      cookiesObj[splitCookie[0]] = splitCookie[1];
+    }
+  }
+  req.cookies = cookiesObj;
+  next();
 };
 
 module.exports = parseCookies;
