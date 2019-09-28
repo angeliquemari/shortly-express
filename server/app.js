f2ba7dc61933cec50d520efd1e508cb6a0851a04
angeliquemari <angelique@nehmzow.com>
2019-09-27 17:30:12 -0700

    Implement User Registration, Login with Successful Redirects

--- server/app.js
+++ server/app.js
@@ -17,68 +17,112 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 
 
-app.get('/', 
-(req, res) => {
-  res.render('index');
-});
+app.get('/',
+  (req, res) => {
+    res.render('index');
+  });
 
-app.get('/create', 
-(req, res) => {
-  res.render('index');
-});
+app.get('/create',
+  (req, res) => {
+    res.render('index');
+  });
 
-app.get('/links', 
-(req, res, next) => {
-  models.Links.getAll()
-    .then(links => {
-      res.status(200).send(links);
-    })
-    .error(error => {
-      res.status(500).send(error);
-    });
-});
-
-app.post('/links', 
-(req, res, next) => {
-  var url = req.body.url;
-  if (!models.Links.isValidUrl(url)) {
-    // send back a 404 if link is not valid
-    return res.sendStatus(404);
-  }
-
-  return models.Links.get({ url })
-    .then(link => {
-      if (link) {
-        throw link;
-      }
-      return models.Links.getUrlTitle(url);
-    })
-    .then(title => {
-      return models.Links.create({
-        url: url,
-        title: title,
-        baseUrl: req.headers.origin
+app.get('/links',
+  (req, res, next) => {
+    models.Links.getAll()
+      .then(links => {
+        res.status(200).send(links);
+      })
+      .error(error => {
+        res.status(500).send(error);
       });
-    })
-    .then(results => {
-      return models.Links.get({ id: results.insertId });
-    })
-    .then(link => {
-      throw link;
-    })
-    .error(error => {
-      res.status(500).send(error);
-    })
-    .catch(link => {
-      res.status(200).send(link);
-    });
-});
+  });
+
+app.post('/links',
+  (req, res, next) => {
+    var url = req.body.url;
+    if (!models.Links.isValidUrl(url)) {
+      // send back a 404 if link is not valid
+      return res.sendStatus(404);
+    }
+
+    return models.Links.get({ url })
+      .then(link => {
+        if (link) {
+          throw link;
+        }
+        return models.Links.getUrlTitle(url);
+      })
+      .then(title => {
+        return models.Links.create({
+          url: url,
+          title: title,
+          baseUrl: req.headers.origin
+        });
+      })
+      .then(results => {
+        return models.Links.get({ id: results.insertId });
+      })
+      .then(link => {
+        throw link;
+      })
+      .error(error => {
+        res.status(500).send(error);
+      })
+      .catch(link => {
+        res.status(200).send(link);
+      });
+  });
 
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
 
+app.get('/signup',
+  (req, res) => {
+    res.render('signup');
+  });
 
+app.post('/signup',
+  (req, res) => {
+    models.Users.create({username: req.body.username, password: req.body.password})
+      .then(() => {
+        res.redirect('/');
+      })
+      .catch(() => {
+        res.redirect('/login');
+      });
+  });
+
+app.get('/login',
+  (req, res) => {
+    res.render('login');
+  });
+
+app.post('/login',
+  (req, res) => {
+    models.Users.get({username: req.body.username})
+      .then((rowObj) => {
+        if (rowObj) {
+          return models.Users.compare(req.body.password, rowObj.password, rowObj.salt);
+        } else {
+          // res.send('Invalid username');
+          throw Error('Invalid username');
+        }
+      })
+      .then((isAuthenticated) => {
+        if (isAuthenticated) {
+          res.redirect('/');
+        } else {
+          // res.send('Invalid password');
+          throw Error('Invalid password');
+        }
+      })
+      .catch((err) => {
+        // res.send(err);
+        res.set({location: '/login'}).send(err);
+      });
+  });
 
 /************************************************************/
 // Handle the code parameter route last - if all other routes fail
